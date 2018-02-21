3bd306c469984275dd8c8b1154d537ab64c49855
sandeepkrjsr <sandeepkrjsr1@gmail.com>
2018-02-22 03:23:37 +0530

    2

---
+++ app/src/main/java/com/ecell/esummit/Dashboard.kt
@@ -0,0 +1,36 @@
+package com.ecell.esummit
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+
+class Dashboard : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard)
+    }
+
+    fun Technology(v: View){
+        get_schedule("1")
+    }
+
+    fun Innovation(v: View){
+        get_schedule("2")
+    }
+
+    fun Business(v: View){
+        get_schedule("3")
+    }
+
+    fun Startup(v: View){
+        get_schedule("4")
+    }
+
+    fun get_schedule(track: String){
+        val intent = Intent(baseContext, Tracks::class.java)
+        intent.putExtra("track", track)
+        startActivity(intent)
+    }
+}
