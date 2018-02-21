47aff3c2f476e8bcc498fd081486b88ef8932355
sandeepkrjsr <sandeepkrjsr1@gmail.com>
2018-02-21 17:16:13 +0530

    second commit

---
+++ app/src/main/java/com/ecell/e_summit18/Adapter.kt
@@ -0,0 +1,52 @@
+package com.ecell.e_summit18
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * Created by 1505560 on 21-Feb-18.
+ */
+internal class Adapter(private val array_time: List<String>, private val array_title: List<String>, private val array_venue: List<String>, private val array_description: List<String>, private val array_speaker: List<String>, private val array_profile: List<String>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
+
+    private var time: TextView? = null
+    private var title: TextView? = null
+    private var venue: TextView? = null
+    private var description: TextView? = null
+    private var speaker: TextView? = null
+    private val profile: TextView? = null
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        init {
+
+            time = view.findViewById(R.id.time) as TextView
+            title = view.findViewById(R.id.title) as TextView
+            venue = view.findViewById(R.id.venue) as TextView
+            description = view.findViewById(R.id.description) as TextView
+            speaker = view.findViewById(R.id.speaker) as TextView
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.card, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        time!!.text = array_time[position]
+        title!!.text = array_title[position]
+        venue!!.text = array_venue[position]
+        description!!.text = array_description[position]
+        speaker!!.text = array_speaker[position]
+        //        profile.setText(array_profile.get(position));
+    }
+
+    override fun getItemCount(): Int {
+        return array_title.size
+    }
+}
