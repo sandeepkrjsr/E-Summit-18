85e3641d4aca1ff8f81822b87103a272b4af01d0
sandeepkrjsr <sandeepkrjsr1@gmail.com>
2018-02-22 03:39:50 +0530

    3

--- app/src/main/java/com/ecell/esummit/Day.java
+++ app/src/main/java/com/ecell/esummit/Day.java
@@ -40,7 +40,7 @@ public class Day extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.day, container, false);
+        View view = inflater.inflate(R.layout.recycler, container, false);
         array_time = new ArrayList<>();
         array_title = new ArrayList<>();
         array_venue = new ArrayList<>();
